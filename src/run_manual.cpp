ffc538d5dc717253c81dbea0d125344337d387f4
brain0071 <18341314091brain@gmail.com>
2025-07-07 12:27:32 +0800

    0707

--- src/run_manual.cpp
+++ src/run_manual.cpp
@@ -14,23 +14,118 @@ class ManualControlNode : public rclcpp::Node {
   public:
   ManualControlNode() : Node("manual_control_node")
   {
-    subscription_ = this->create_subscription<mavros_msgs::msg::Mavlink>(
-      "/uas1/mavlink_sink", rclcpp::QoS(10).best_effort(), [this](const mavros_msgs::msg::Mavlink::SharedPtr rmsg) {
+    mavlink_sub = this->create_subscription<mavros_msgs::msg::Mavlink>(
+      "/uas1/mavlink_source", rclcpp::QoS(10).best_effort(), [this](const mavros_msgs::msg::Mavlink::SharedPtr rmsg) {
     this->mavlinkCallback(rmsg);});
+
+    joystick_pub = this->create_publisher<mavros_msgs::msg::ManualControl>(
+    "/joystick", 10);  
+
   }
 
-  private:
-  rclcpp::Subscription<mavros_msgs::msg::Mavlink>::SharedPtr subscription_;
 
+  private:
+
+  rclcpp::Subscription<mavros_msgs::msg::Mavlink>::SharedPtr mavlink_sub;
+  rclcpp::Publisher<mavros_msgs::msg::ManualControl>::SharedPtr joystick_pub;
 
   void mavlinkCallback(const mavros_msgs::msg::Mavlink::SharedPtr rmsg)
   {
-    RCLCPP_INFO(this->get_logger(), "Update");
     mavlink_message_t mmsg;
     convert(*rmsg, mmsg);
     if (mmsg.msgid == MAVLINK_MSG_ID_MANUAL_CONTROL) 
     {
-      RCLCPP_INFO(this->get_logger(), "Received MAVLINK_MSG_ID_MANUAL_CONTROL (msgid = %d)", mmsg.msgid);
+      // RCLCPP_INFO(this->get_logger(), "Received MAVLINK_MSG_ID_MANUAL_CONTROL (msgid = %d)", mmsg.msgid);
+      mavlink_manual_control_t packet;
+      mavlink_msg_manual_control_decode(&mmsg, &packet);
+      mavros_msgs::msg::ManualControl manual_msg;
+      manual_msg.header.stamp = this->now();
+      manual_msg.x = packet.x / 1000.0;
+      manual_msg.y = packet.y / 1000.0;
+      manual_msg.z = packet.z / 1000.0;
+      manual_msg.r = packet.r / 1000.0;
+      manual_msg.buttons = packet.buttons;
+      manual_msg.buttons2 = packet.buttons2;
+      manual_msg.enabled_extensions = packet.enabled_extensions;
+      manual_msg.s = packet.s / 1000.0;
+      manual_msg.t = packet.t / 1000.0;
+      manual_msg.aux1 = packet.aux1 / 1000.0;
+      manual_msg.aux2 = packet.aux2 / 1000.0;
+      manual_msg.aux3 = packet.aux3 / 1000.0;
+      manual_msg.aux4 = packet.aux4 / 1000.0;
+      manual_msg.aux5 = packet.aux5 / 1000.0;
+      manual_msg.aux6 = packet.aux6 / 1000.0;
+      joystick_pub->publish(manual_msg);
+      
+      switch (packet.buttons) {
+        
+        // D-pad
+        // up(6144): Roll+
+        // down(8192): Roll- 
+        // left(16384): Pitch+ 
+        // right(32768): Pitch-
+        case 6144: {
+          RCLCPP_INFO(this->get_logger(), "Roll+");
+          break; 
+        }
+    
+        case 8192: { 
+          RCLCPP_INFO(this->get_logger(), "Roll-");
+          break; 
+        }
+
+        case 16384: { 
+          RCLCPP_INFO(this->get_logger(), "Pitch+");
+          break; 
+        }
+    
+        case 32768: { 
+          RCLCPP_INFO(this->get_logger(), "Pitch-");
+          break; 
+        }
+
+        // Button
+        // X(4):Yaw+ 
+        // B(2):Yaw-  
+        // Y(8):Z+ 
+        // A(1):Z-
+        case 4: {
+          RCLCPP_INFO(this->get_logger(), "Yaw+");
+          break; 
+        }
+    
+        case 2: { 
+          RCLCPP_INFO(this->get_logger(), "Yaw-");
+          break; 
+        }
+
+        case 8: { 
+          RCLCPP_INFO(this->get_logger(), "Z+");
+          break; 
+        }
+    
+        case 1: { 
+          RCLCPP_INFO(this->get_logger(), "Z-");
+          break; 
+        }
+
+        // gripper 
+        // left(512): open 
+        // right(1024): close
+        case 512: {
+          RCLCPP_INFO(this->get_logger(), "Gripper open");
+          break; 
+        }
+    
+        case 1024: { 
+          RCLCPP_INFO(this->get_logger(), "Gripper close");
+          break; 
+        }
+        
+        default: 
+          break;
+        }
+
     }
   }
 
